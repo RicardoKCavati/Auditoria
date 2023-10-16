532b490c1cc969ccc73736fc4587277d2b2096d0
RicardoKCavati <ricardokcavati@gmail.com>
2023-10-16 09:33:51 -0300

    Adicionar projetos client e server.

---
+++ Auditoria/Server/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auditoria.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("test")]
+        public IEnumerable<string> TestEndpoint(bool isName)
+        {
+            if (isName)
+            {
+                return new string[] { "nome1", "nome2", "nome3" };
+            }
+
+            return new string[] { "test1", "test2", "test3" };
+        }
+    }
+}
